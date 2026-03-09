a11a045aa70f157692d4b694f694f7b3b523d9fd
rube-de <rube@ruf2.de>
2026-03-09 17:16:35 +0100

    fix: comment sapphire-hardhat to be able verify contract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@oasisprotocol/sapphire-hardhat";
+// import "@oasisprotocol/sapphire-hardhat";
 import * as dotenv from "dotenv";
 
 dotenv.config();
